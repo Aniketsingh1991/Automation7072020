a5bf93c1ad5daa56f849239f6b57b310f3efd26b
Aniketsingh1991 <aniket.s.rajput@gmail.com>
2020-07-31 01:02:57 -0700

    Basic Selenium

---
+++ BasicSeleniumFunctions/src/SeleniumFunction/LaunchBrowser.java
@@ -0,0 +1,35 @@
+package SeleniumFunction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	
+	public static void main(String[] args) {
+		
+		//Set Chrome drivers path
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\Drivers\\chromedriver.exe");
+		
+		ChromeDriver dr= new ChromeDriver();
+		
+		dr.manage().window().maximize();
+		
+		dr.get("http://sampleapp.tricentis.com/101/index.php");
+		
+		//dr.findElement(By.id("nav_automobile")).click();
+		
+				WebElement ele = dr.findElement(By.id("nav_automobile"));
+				
+				String data = ele.getText();
+			    System.out.println(data);
+			    ele.click();
+			    
+			    //enter text
+			    
+			    dr.findElement(By.id("search_form")).sendKeys("VW");
+			    dr.findElement(By.id("search_form")).sendKeys(Keys.ENTER);
+	}
+}
